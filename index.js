03928db88a03585a4222490390837e0a79f0aa86
bernard frimpong <nardfrimpong@gmail.com>
2021-10-15 17:45:44 +0000

    changed port to local

--- index.js
+++ index.js
@@ -2,13 +2,15 @@ import dotenv from 'dotenv'
 import express from 'express';
 import mongoose from 'mongoose';
 import TodoModel from './schemas/todo_schema.js';
+import cors from 'cors';
 
 dotenv.config()
 
 const app = express();
 app.use(express.json());
+app.use(cors);
 
-const port = 3000 
+const port = 3000 || process.env.port 
 const db = process.env.DB_URL
 
 mongoose.connect(db, {
@@ -37,8 +39,10 @@ app.get('/', (req, res) => {
 })
 
 ///get all todos
-app.get('/todos',async (req,res)=>{
-const todoModel = await TodoModel.find({});
+app.get('/todos/:status',async (req,res)=>{
+
+const {status} = req.params;
+const todoModel = await TodoModel.find({}).where('status').equals(status);
 if(todoModel){
     return res.status(200).json({
         status:true,
